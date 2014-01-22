454315539997f729c48646b4ac79736d4a11f87a
Scott West <scott.gregory.west@gmail.com>
2014-01-22 22:23:44 +0100

    Adding flycheck clang options to dir-locals.

--- src/.dir-locals.el
+++ src/.dir-locals.el
@@ -1,1 +1,7 @@
-((nil . ((compile-command . "make -C ../build -k"))))
+((nil
+  . ((compile-command . "make -C ../build -k")))
+ (c++-mode
+  . ((flycheck-clang-include-path
+      . ("/home/scott/local/Eiffel_14.05/studio/spec/linux-x86-64/include/"
+	 "./"))
+     (flycheck-clang-language-standard . "c++11"))))
